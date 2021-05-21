49196f63342c2388019c63035dd34f8cea86a749
Shrinidhi Holla <hollashrinidhi99@gmail.com>
2021-05-21 22:50:45 +0530

    assignment 3 challenge commit

---
+++ growing text (assignment3)/Challenge/script.js
@@ -0,0 +1,27 @@
+var var1 = setInterval(inTimer, 100);
+var fs = 10; //font size
+var ids = document.getElementById("growtext");
+
+
+function inTimer() {
+    ids.innerHTML = 'Just shoot!!!!';
+    ids.setAttribute('style', "font-size: " + fs + "px; color: white");
+    fs += 5;
+    if (fs >= 50) {
+        clearInterval(var1); //paused
+        var2 = setInterval(deTimer, 100);
+
+    }
+    deTimer();
+}
+
+function deTimer() //fs = 50px
+{
+    fs -= 5;
+    ids.innerHTML = 'Just shoot!!!!';
+    ids.setAttribute('style', "font-size: " + fs + "px; color: white");
+    if (fs == 10) {
+        clearInterval(var2); //paused
+        var1 = setInterval(inTimer, 100);
+    }
+}
